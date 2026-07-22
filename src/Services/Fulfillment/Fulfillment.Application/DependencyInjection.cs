5ed738a65e1e66fbe056c0d6eeae324d25b600d5
baris <barisozay01@gmail.com>
2026-07-22 03:20:54 +0300

    feat(fulfillment): implement domain, consumers, infrastructure, and api host

---
+++ src/Services/Fulfillment/Fulfillment.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+using FluentValidation;
+using Fulfillment.Application.Common.Behaviors;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fulfillment.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        });
+
+        return services;
+    }
+}
